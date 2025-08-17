1d8a2660fd2dfb64c32e590c6eafb992c8ebcba4
Leslie <leslie.bagalho@yahoo.com.br>
2025-08-17 16:43:40 -0300

    feat: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App(){
   return(
     <div>
       <h1>Hola Mundo</h1>
+      <p>Executando a minha primeira alteração para ser enviado ao repositório</p>
     </div>
   )
 }
